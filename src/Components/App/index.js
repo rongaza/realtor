2d7bbc13774a80da03e6ae8166c2caf9319cabb5
Ron Gaza <rongaza@gmail.com>
2020-07-24 19:21:12 -0700

    added basic navigation and routing

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navigation from '../Navigation'
+import Landing from '../Landing'
+import SignUp from '../SignUp'
+import SignIn from '../SignIn'
+import Dashboard from '../Dashboard'
+import Account from '../Account'
+
+import * as ROUTES from '../../constants/routes'
 
 function App() {
-  return (
-    <div>
-      <h1>Realtor App</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Navigation />
+
+                <hr />
+
+                <Route exact path={ROUTES.LANDING} component={Landing} />
+                <Route path={ROUTES.SIGN_UP} component={SignUp} />
+                <Route path={ROUTES.SIGN_IN} component={SignIn} />
+                <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+                <Route path={ROUTES.ACCOUNT} component={Account} />
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+export default App
